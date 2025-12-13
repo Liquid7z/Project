e978c18be4138f97cbcf5d05a28e670e8379b766
Liquid <anshbhangi66@gmail.com>
2025-12-13 00:17:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3951075674-40672",
+  "appId": "1:566361186986:web:1f36142c584f21368b26d3",
+  "apiKey": "AIzaSyBmTWeCeWHXPn_wtIvwGUGxYcHwPSBqwng",
+  "authDomain": "studio-3951075674-40672.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "566361186986"
+};
