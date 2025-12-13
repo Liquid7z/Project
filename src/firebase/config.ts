22ebaa9a08e287b591704dc774f9a81642f99123
Liquid <anshbhangi66@gmail.com>
2025-12-13 17:43:26 +0000

    storageBucket: "studio-3951075674-40672.firebasestorage.app"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:566361186986:web:1f36142c584f21368b26d3",
   "apiKey": "AIzaSyBmTWeCeWHXPn_wtIvwGUGxYcHwPSBqwng",
   "authDomain": "studio-3951075674-40672.firebaseapp.com",
+  "storageBucket": "studio-3951075674-40672.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "566361186986"
 };
