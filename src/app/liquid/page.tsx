46f214ace6d76e658042604f5b02f01c7d32c33d
Liquid <anshbhangi66@gmail.com>
2025-12-15 20:39:35 +0000

    Try fixing this error: `Runtime ReferenceError: where is not defined. Er

--- src/app/liquid/page.tsx
+++ src/app/liquid/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useUser, useFirestore, useCollection, useDoc, useMemoFirebase } from '@/firebase';
-import { collection, query, orderBy, doc, updateDoc, writeBatch, setDoc, deleteDoc } from 'firebase/firestore';
+import { collection, query, where, orderBy, doc, updateDoc, writeBatch, setDoc, deleteDoc } from 'firebase/firestore';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
